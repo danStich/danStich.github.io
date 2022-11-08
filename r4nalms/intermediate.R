9cf41cb2d9b61dc71293cff0ea5b2b6430737fd3
danStich <daniel.stich@oneonta.edu>
2022-11-07 19:00:14 -0500

    update tutorial code

--- r4nalms/intermediate.R
+++ r4nalms/intermediate.R
@@ -1,238 +1,68 @@
 
-## ---------------------------------------------
 library(tidyverse)
 
 
-
-## -----------------------------------------------------------------------------
-minnesota <- read.csv("data/minnesota.csv", stringsAsFactors = FALSE)
+otsego <- read.csv("data/physical.csv", stringsAsFactors = FALSE)
 
 
-
-## -------------------------------------------------------------------
 # Like this:
-str(minnesota)
+glimpse(otsego)
 
 
-
-## -----------------------------------------------------------------------------
-names(minnesota)
+names(otsego)
 
 
+# First, we replace temp with Temperature
+names(otsego)[3] <- "Temperature"
 
-## -----------------------------------------------------------------------------
-# First, we replace Secchi..m. with Secchi
-names(minnesota)[8] <- "Secchi"
-
-
-## -----------------------------------------------------------------------------
 # Print the names of the df
 # to the console so we can
 # see them.
-names(minnesota)
+names(otsego)
+
+library(lubridate)
+
+# Get date in a standard, unambiguous format
+otsego$date <- as.Date(otsego$date, format = "%m/%d/%Y")
+
+# Make a variable to hold year in the df
+otsego$year <- year(otsego$date)
+
+# Make a variable to hold month
+otsego$month_num <- month(otsego$date)
+
+# Convert this to a factor so it can be easily re-leveled and ordered
+otsego$month <- month.abb[otsego$month_num]
+otsego$month <- factor(otsego$month, levels = month.abb)
+
+# Let's make an ordinal date variable to get day of year as well
+otsego$doy <- yday(otsego$date)
 
 
-## -----------------------------------------------------------------------------
-# So funny
-minnesummary <- minnesota %>%
-  group_by(Water.Body, GNIS.ID, GNIS.Class) %>%
-  summarize(
-    Secchi = mean(Secchi),
-    Latitude = mean(Latitude),
-    Longitude = mean(Longitude),
-    .groups = "keep"
-  )
+ggplot(otsego, aes(x = Temperature)) +
+  geom_histogram(bins = 30)
 
+otsego$log_temperature <- log(otsego$Temperature)
 
-## ------------------------------------------------
-library(rgdal)
-MN <- rgdal::readOGR("data/Boundaries_of_Minnesota.shp", verbose = FALSE)
+ggplot(otsego, aes(x = log_temperature)) +
+  geom_histogram(bins = 30)
 
+otsego <- otsego %>% 
+  filter(!is.na(Temperature) & Temperature > 0)
 
-## -----------------------------------------------------------------------------
-# Remove those points with missing longitude and latitude
-d <- minnesummary %>%
-  filter(!is.na(Latitude) &
-    !is.na(Longitude) &
-    Longitude <= 0 &
-    (Latitude >= 40 & Latitude <= 60))
+otsego$layer <- "epilimnion"
+otsego$layer[otsego$depth > 15] <- "hypolimnion" 
 
-
-## -----------------------------------------------------------------------------
-# Load sp package
-library(sp)
-
-# Make an intermediate object to modify
-ds <- d
-
-# Assign longitude and latitude to a
-# SpatialPoints-class obj. This converts d to
-# a spatial dataframe
-coordinates(ds) <- c("Longitude", "Latitude")
-
-
-## -----------------------------------------------------------------------------
-proj4string(ds) <- CRS("+proj=longlat +datum=WGS84")
-
-
-## -----------------------------------------------------------------------------
-# Get UTMs for the longitudes and latitudes using
-# the coordinate system of our shape file
-coord_utm <- sp::spTransform(ds, CRS("+init=epsg:26911"))
-
-
-## -----------------------------------------------------------------------------
-# Assign the coordinates to new columns
-# in our dataframe
-d$x <- coord_utm@coords[, 1]
-d$y <- coord_utm@coords[, 2]
-coordinates(d) <- ~ x + y
-proj4string(d) <- CRS("+init=epsg:26911")
-
-
-## -----------------------------------------------------------------------------
-MN <- sp::spTransform(MN, CRS("+init=epsg:26911"))
-
-
-## ------------------------------------------------------------
-# We'll use the ggplot2 library
-library(ggplot2)
-
-# Make the plot
-ggplot() +
-  geom_polygon(
-    data = fortify(MN),
-    color = "black",
-    fill = "gray40",
-    aes(x = long, y = lat)
-  ) +
-  # coord_sf() +
-  geom_point(
-    data = data.frame(d),
-    mapping = aes(x = x, y = y)
-  ) +
-  labs(x = "Easting", y = "Northing") +
-  ggtitle("Minnesota lakes") +
-  theme(
-    plot.title = element_text(hjust = .5),
-    text = element_text(size = 10)
-  ) +
-
-  # Adjusting output width: distorts CRS
-  # but can actually see the plot
-  coord_equal(ratio = .5)
-
-
-## -----------------------------------------------------------------------------
-# Perform a spatial intersect between
-# the Minnesota shapefile (polygon) and
-# the SpatialPoints object.
-# Note that order is important here.
-proj4string(d) <- proj4string(MN)
-ins <- sp::over(d, MN)
-
-# Then, we can drop the points that
-# do not not intersect with the polygon,
-# now saving over the original data set.
-dd <- d[!is.na(ins[, 1]), ]
-
-# Get the modified data back out of the
-# SpatialPoints object
-minnesota_sp <- dd@data
-
-
-## ----------------------------------------------------------------
-# We'll use the ggplot2 library
-library(ggplot2)
-
-# You may need to install sf library with 
-# install.packages("sf") on some OS
-
-# Make the plot
-ggplot() +
-  geom_polygon(
-    data = fortify(MN),
-    color = "black",
-    fill = "gray60",
-    aes(x = long, y = lat)
-  ) +
-  coord_sf() +
-  geom_point(
-    data = data.frame(dd),
-    mapping = aes(x = x, y = y)
-  ) +
-  labs(x = "Easting", y = "Northing") +
-  ggtitle("Minnesota lakes") +
-  theme(
-    plot.title = element_text(hjust = .5),
-    text = element_text(size = 10)
-  )
-
-
-## ----------------------------------------------------------------
-# Make the plot...again
-ggplot() +
-  geom_polygon(
-    data = fortify(MN),
-    color = "black",
-    fill = "gray90",
-    aes(x = long, y = lat)
-  ) +
-  coord_sf() +
-  geom_jitter(
-    data = data.frame(dd),
-    mapping = aes(x = x, y = y, color = dd$Secchi),
-    alpha = .5, size = 3
-  ) +
-  labs(x = "", y = "") +
-  ggtitle("Secchi depth (m)") +
-  theme(
-    plot.title = element_text(hjust = .5, face = "bold"),
-    text = element_text(size = 14),
-    legend.position = "right",
-    legend.title.align = 0,
-    panel.background = element_blank(),
-    axis.ticks.y = element_blank(),
-    axis.text.y = element_blank(),
-    axis.ticks.x = element_blank(),
-    axis.text.x = element_blank()
-  ) +
-  scale_colour_gradientn("", colours = c("gray90", "black"))
-
-
-## -----------------------------------------------------------------------------
-ggplot(minnesummary, aes(x = Secchi)) +
-  geom_histogram(bins = 15)
-
-
-## -----------------------------------------------------------------------------
-minnesummary$logSecchi <- log(minnesummary$Secchi)
-
-
-## -----------------------------------------------------------------------------
-ggplot(minnesummary, aes(x = logSecchi)) +
-  geom_histogram(bins = 15)
-
-
-## -----------------------------------------------------------------------------
-# Haha, like the abbreviation for Holdren et al. (2006)!
-mlr <- minnesummary %>%
-  filter(GNIS.Class == "Lake" | GNIS.Class == "Reservoir")
-
-
-## -----------------------------------------------------------------------------
 # Wilcox test to assess the null hypothesis
-# that there is no difference in Secchi between
-# lakes and reservoirs.
-wilcox.test(Secchi ~ GNIS.Class, data = mlr)
+# that there is no difference in temperature between
+# epilimnion and hypolimnion.
+wilcox.test(Temperature ~ layer, data = otsego)
 
-
-## -----------------------------------------------------------------------------
 # T-test to assess the null hypothesis
-# that there is no difference in Secchi
-# between lakes and reservoirs in Minnesota.
+# that there is no difference in temperature
+# between the epilimnion and hypolimnion in Otsego Lake.
 
-# We use logSecchi to meet assumptions
+# We use log_temperature to meet assumptions
 # of normality.
 
 # We can specify this one using a formula.
@@ -243,75 +73,41 @@ wilcox.test(Secchi ~ GNIS.Class, data = mlr)
 # are not equal, and this defaults to
 # a Welch's t-test that uses a calculated df
 # to adjust the calculated test statistic.
-t.test(logSecchi ~ GNIS.Class, data = mlr, equal = FALSE)
+t.test(log_temperature ~ layer, data = otsego, equal = FALSE)
 
+mod <- t.test(log_temperature ~ layer, data = otsego, equal = FALSE)
 
-## -------------------------------------
-mod <- t.test(logSecchi ~ GNIS.Class, data = mlr, equal = FALSE)
+ggplot(data = otsego,
+       aes(x = layer, y = log_temperature, color = layer, fill = layer)) +
+  geom_violin(alpha = 0.10, draw_quantiles = 0.50)
 
-
-## -----------------------------------------------------------------------------
-ggplot(
-  data = mlr,
-  aes(
-    x = GNIS.Class, y = logSecchi,
-    color = GNIS.Class, fill = GNIS.Class
-  )
-) +
-  geom_violin(alpha = 0.10, draw_quantiles = 0.50) +
-  geom_jitter(width = 0.1, alpha = 0.20)
-
-
-## -----------------------------------------------------------------------------
 # Fit an ANOVA to test for differences in
 # means between groups
-mod <- lm(logSecchi ~ GNIS.Class, data = mlr)
+mod <- lm(log(Temperature) ~ month, data = otsego)
 
-
-## -----------------------------------------------------------------------------
 # Get ANOVA summary for the model
 anova(mod)
 
+ggplot(otsego, aes(x = month, y = Temperature)) +
+  geom_boxplot()
 
-## -----------------------------------------------------------------------------
-TukeyHSD(aov(mod))
+# Fit the model
+lmod <- lm(log(Temperature) ~ year, data = otsego)
 
-
-## -----------------------------------------------------------------------------
-# Drop rows with missing Latitudes
-mn_data <- minnesummary %>%
-  filter(!is.na(Latitude) &
-    (Latitude >= 40 & Latitude <= 50))
-
-# Now fit the model
-lmod <- lm(logSecchi ~ Latitude, data = mn_data)
-
-
-## -----------------------------------------------------------------------------
 summary(lmod)
 
-
-## --------------------------------------------
 log_preds <- predict(lmod, interval = "prediction")
 
-
-## -----------------------------------------------------------------------------
 # Convert back to real scale
 real_preds <- apply(X = log_preds, MARGIN = 2, FUN = exp)
 
-
-## -----------------------------------------------------------------------------
 # Take a look at the first few
 # rows of the preds dataframe
 head(real_preds)
 
+otsego_preds <- data.frame(otsego, real_preds)
 
-## -----------------------------------------------------------------------------
-mn_preds <- data.frame(mn_data, real_preds)
-
-
-## -----------------------------------------------------------------------------
-ggplot(mn_preds, aes(x = Latitude, y = Secchi)) +
+ggplot(otsego_preds, aes(x = year, y = Temperature)) +
   geom_point(alpha = 0.10) +
   geom_line(aes(y = fit), size = 1, color = "black") +
   geom_ribbon(aes(ymin = lwr, ymax = upr),
@@ -323,47 +119,45 @@ ggplot(mn_preds, aes(x = Latitude, y = Secchi)) +
   ) +
   theme(legend.position = "none")
 
+surface <- otsego %>% 
+  filter(depth <= 5)
 
-## -----------------------------------------------------------------------------
-otsego <- read.csv("data/physical.csv")
+surface_mod <- lm(log(Temperature) ~ year + month, data = surface) 
+
+# Make predictions from the model on the log scale
+lpreds <- predict(surface_mod, interval = "prediction")
+
+# Back-transform fit, lwr, upr to the real scale by exponentiating
+rpreds <- apply(lpreds, 2, exp)
+
+# Smash it together with the original data.
+# Yes R, we know these are not future responses, just do it
+lake_preds <- data.frame(surface, rpreds)
+
+# Plot the predictions against the observed data
+ggplot(lake_preds, aes(x = year, y = Temperature)) +
+  geom_point(alpha = 0.10) +
+  geom_line(aes(y = fit)) +
+  geom_ribbon(aes(xmax = year, ymin = lwr, ymax = upr), alpha = 0.1) +
+  facet_wrap(~month) +
+  theme_bw()
+
+# Set this aside as a new object
+# We'll drop any observations that lack data
+lim <- otsego %>% 
+  filter(!is.na(depth) & !is.na(doy) & !is.na(Temperature))
 
 
-## -----------------------------------------------------------------------------
-# Data formatting & extraction
-
-# First, we convert the date column
-# to a character string. We pass the
-# result directly to the as.Date
-# function, and along with that we
-# specify a format so R knows where it
-# is looking for specific elements of
-# the date info we are trying to pass.
-otsego$date <- as.Date(
-  as.character(otsego$date),
-  format = "%m/%d/%Y"
-)
-
-
-## -----------------------------------------------------------------------------
-# Remove NA values to make life easier
-lim <- na.omit(otsego)
-
-
-## -----------------------------------------------------------------------------
 # Multiply depth column by -1 so depth will
 # plot from top to bottom.
 lim$depth <- -1 * lim$depth
 
-
-## ------------------------------------------------
 library(akima)
 
 
-
-## -----------------------------------------------------------------------------
 # Create a data frame containing the
 # x, y, and z variables of interest
-plotter <- data.frame(x = lim$date, y = lim$depth, z = lim$temp)
+plotter <- data.frame(x = lim$doy, y = lim$depth, z = lim$Temperature)
 
 # Sort it so we have ascending values of x and y
 plotter <- plotter[with(plotter, order(x, y)), ]
@@ -374,13 +168,11 @@ im <- with(
   plotter,
   akima::interp(x, y, z,
     duplicate = "mean",
-    nx = length(unique(lim$date)),
-    ny = length(unique(lim$depth))
+    nx = 360,#length(unique(lim$doy)),
+    ny = 50 #length(unique(lim$depth))
   )
 )
 
-
-## -----------------------------------------------------------------------------
 # Plot the isopleth
 # filled.contour is the function that actually
 # makes the contour plot. This is the same function
@@ -402,14 +194,14 @@ par(mar = c(4, 4, 2, 8))
 filled.contour(
   im$x, # Variable on x-axis (date)
   im$y, # Variable on y-axis (depth)
-  im$z, # Response (wq parameter)
+  im$z, # Response (water quality parameter)
   # Could also choose 'grey.colors' or 'terrain.colors'.
   # If you want the ramp to go the other way,
   # just delete the 'rev'. Note that you will
   # need to change the 26 in parentheses to match
   # the number of levels that you actually have or
   # want to display.
-  col = topo.colors(26),
+  col = topo.colors(30),
   # I don't like in-figure titles.
   # You can add one, though. You will, however,
   # need to change the 'mar' argument in the call
@@ -420,45 +212,31 @@ filled.contour(
   # Specify x-axis limits. In this case, we are "zooming in" on year 2017
   # Can use this one instead if date formatting throws error
   # xlim = c(as.numeric(as.Date("2017/05/01")), max(im$x)),
-  xlim = c(as.Date("2017/05/01"), max(im$x)),
+  xlim = c(0, 366),
   # X-axis label
-  xlab = "Date",
+  xlab = "Day of Year",
   # Y-axis label
   ylab = "Depth (m)",
-  # Axis options
+  # This next block of code controls the lines that are overlaid. The interpolation
+  # is the same, so the axes and the number of levels need to be the same, too.
   plot.axes = {
-    # This is how we include
-    # countour lines
     contour(
-      im$x,
-      im$y,
-      im$z,
-      nlevels = 26,
-      drawlabels = FALSE,
-      col = topo.colors(26),
-      lwd = 1,
-      lty = 2,
-      add = TRUE
+      im$x, im$y, im$z, # X, Y, Z values (same)
+      nlevels = 26, # Need to change this with the No. on line 37 to be same!
+      levels = seq(0, 26, 1), # Will need to change for different parameters
+      drawlabels = FALSE, # Don't want R to draw the labels, they are ugly
+      col = rgb(1,1,1,0),
+      add = TRUE); # Add the lines to the existing plot
+    # X-axis to assign month abbreviation at first ordinal day of each month
+    axis(1, at = c(1, 32, 61, 92, 122, 153, 183, 214, 245, 275, 306, 336),
+         labels = month.abb, cex.axis=1.10
     )
     # Y-axis
-    axis(2,
-      at = seq(0, -50, -10),
-      labels = seq(0, 50, 10)
-    )
-    # X-axis
-    axis(1,
-      at = seq(as.Date("2017/05/01"),
-        by = "2 months",
-        length.out = 16
-      ),
-      labels = format(
-        seq(as.Date("2017/05/01"),
-          by = "2 months",
-          length.out = 16
-        ),
-        "%b %Y"
-      )
-    )
-  }
-)
-
+    ### Change to match range of data
+    axis(side=2, at=seq(-50, 0, 5), seq(50, 0, -5), las=2, yaxt='l', cex.axis=1.1);
+    # Add sampling points to x-axis
+    points(unique(lim$doy), 
+           rep(-50, length(unique(lim$doy))), 
+           pch=21, bg=rgb(0,0,0,.05), xpd=T)
+    
+  })
